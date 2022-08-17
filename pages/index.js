38f81f481a57b2e3b2678f5b056047fa3476a2db
Meir <mikonek.a@gmail.com>
2022-08-18 00:50:58 +0600

    last

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="main-container">
       <Head>
-        <title>Create Next App</title>
+        <title>Posts App</title>
       </Head>
       <h1 className="welcome">Welcome to practice posts page</h1>
       <Link href="/posts">
